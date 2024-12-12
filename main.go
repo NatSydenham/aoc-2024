d3fef0fabecb392f3e01a7bb3b86ccc5060d0aa0
Nat Sydenham <nathanial.sydenham@gmail.com>
2024-12-12 23:56:21 +0000

    restore execution

--- main.go
+++ main.go
@@ -1,26 +1,34 @@
 package main
 
 import (
+	"github.com/NatSydenham/aoc-2024/src/day1"
 	"github.com/NatSydenham/aoc-2024/src/day11"
+	"github.com/NatSydenham/aoc-2024/src/day2"
+	"github.com/NatSydenham/aoc-2024/src/day3"
+	"github.com/NatSydenham/aoc-2024/src/day4"
+	"github.com/NatSydenham/aoc-2024/src/day5"
+	"github.com/NatSydenham/aoc-2024/src/day6"
+	"github.com/NatSydenham/aoc-2024/src/day7"
+	"github.com/NatSydenham/aoc-2024/src/day8"
 )
 
 func main() {
-	// day1.ExecutePart1()
-	// day1.ExecutePart2()
-	// day2.ExecutePart1()
-	// day2.ExecutePart2()
-	// day3.ExecutePart1()
-	// day3.ExecutePart2()
-	// day4.ExecutePart1()
-	// day4.ExecutePart2()
-	// day5.ExecutePart1()
-	// day5.ExecutePart2()
-	// day6.ExecutePart1()
-	// day6.ExecutePart2()
-	// day7.ExecutePart1()
-	// day7.ExecutePart2()
-	// day8.ExecutePart1()
-	// day8.ExecutePart2()
+	day1.ExecutePart1()
+	day1.ExecutePart2()
+	day2.ExecutePart1()
+	day2.ExecutePart2()
+	day3.ExecutePart1()
+	day3.ExecutePart2()
+	day4.ExecutePart1()
+	day4.ExecutePart2()
+	day5.ExecutePart1()
+	day5.ExecutePart2()
+	day6.ExecutePart1()
+	day6.ExecutePart2()
+	day7.ExecutePart1()
+	day7.ExecutePart2()
+	day8.ExecutePart1()
+	day8.ExecutePart2()
 	day11.Execute(25)
 	day11.Execute(75)
 }
